1402e2c9375ee8e0a7b4808198c5466a8b55e2be
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2021-04-19 21:16:25 +0100

    Add crypto prices table

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '@crypto-prices/api-interfaces';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'crypto-prices-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   hello$ = this.http.get<Message>('/api/hello');
-  prices$ = this.http.get<any>('/api/prices');
+  prices$ = this.http.get<unknown>('/api/prices');
+  prices = [];
+  Time_Out = 10 * 1000;
+
   constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    // let currencies = [];
+    this.getCrytoPrices();
+
+    // setInterval(() => this.getCrytoPrices(), this.Time_Out)
+
+  }
+
+  private getCrytoPrices() {
+    this.prices = [];
+    this.prices$.pipe(tap(res => {
+      const data = Object.entries(res);
+      data.forEach(([key, value]) => {
+        const item = { key, value: this.getCurrencies(value) };
+        this.prices.push(item);
+      });
+      console.log(this.prices, 'prices ', { res });
+
+    })).subscribe();
+  }
+
+  getCurrencies(value: unknown) {
+    const data = Object.entries(value);
+    const result = [];
+    data.forEach(([key, value]) => {
+      const item = { currency: key, value };
+      result.push(item);
+    })
+    return result;
+  }
 }
--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+// Primeng
+import {TableModule} from 'primeng/table';
+import {TabViewModule} from 'primeng/tabview';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule,TableModule,TabViewModule],
   providers: [],
   bootstrap: [AppComponent],
 })
