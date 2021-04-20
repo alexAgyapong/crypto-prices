8446544af18ad76594892ec241fa12118c6ea37d
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2021-04-20 06:49:28 +0100

    Add error message

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message, Price } from '@crypto-prices/api-interfaces';
-import { tap } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { of, Subscription } from 'rxjs';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'crypto-prices-root',
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit, OnDestroy {
   Time_Out = 10 * 1000;
   subscription: Subscription;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getCrytoPrices();
@@ -26,18 +27,19 @@ export class AppComponent implements OnInit, OnDestroy {
 
   getCrytoPrices() {
     this.prices = [];
-    this.subscription = this.prices$.pipe(tap((res: Price[]) => {
-      const data = Object.entries(res);
-      data.forEach(([coin, currencies]) => {
-        const item = { coin, currencies: this.getCurrencies(currencies) };
-        this.prices.push(item);
-        console.log('second', JSON.stringify(this.prices));
-      });
-      console.log(this.prices, 'prices ', { res });
-      console.log(JSON.stringify(this.prices), 'prices');
-
-
-    })).subscribe();
+    this.subscription = this.prices$
+      .subscribe((res: Price[]) => {
+        const data = Object.entries(res);
+        data.forEach(([coin, currencies]) => {
+          const item = { coin, currencies: this.getCurrencies(currencies) };
+          this.prices.push(item);
+          console.log('second', JSON.stringify(this.prices));
+        });
+        console.log(this.prices, 'prices ', { res });
+        console.log(JSON.stringify(this.prices), 'prices');
+      },
+        (err) => this.getErrorMessage(err)
+      );
   }
 
   getCurrencies(value: unknown) {
@@ -48,6 +50,15 @@ export class AppComponent implements OnInit, OnDestroy {
       result.push(item);
     })
     return result;
+  }
+
+  private getErrorMessage(err: any) {
+    console.log('HTTP Error', err);
+    this.messageService.add({
+      severity: 'info',
+      summary: `We encountered an error retrieving data`
+      // detail: `${err?.statusText}`
+    });
   }
 
   ngOnDestroy() {
--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CryptoPriceListComponent } from './crypto-price-list/crypto-price-list.component';
 
 // Primeng
-import {TableModule} from 'primeng/table';
-import {TabViewModule} from 'primeng/tabview';
-import { CryptoPriceListComponent } from './crypto-price-list/crypto-price-list.component';
+import { TableModule } from 'primeng/table';
+import { MessagesModule } from 'primeng/messages';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [AppComponent, CryptoPriceListComponent],
-  imports: [BrowserModule, HttpClientModule,TableModule,TabViewModule],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, TableModule, MessagesModule],
+  providers: [MessageService],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
