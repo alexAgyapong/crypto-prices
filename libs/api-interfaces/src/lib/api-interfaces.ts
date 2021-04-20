e5d0f5f360216f3d731af8f7e545cf24ea3b6462
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2021-04-20 06:06:10 +0100

    Add price and currency to interfaces

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,13 @@
 export interface Message {
   message: string;
 }
+
+export interface Currency {
+  name: string;
+  value: number;
+}
+export interface Price {
+  coin: string;
+  currencies: Currency[];
+}
+
